c6e9e949af7834dd40e236e34d1f104b7178bc42
mickwald <mickwald@hotmail.com>
2018-10-30 09:18:44 +0100

    Initial commit

---
+++ linuxpintos/src/examples/halt.c
@@ -0,0 +1,16 @@
+/* halt.c
+
+   Simple program to test whether running a user program works.
+ 	
+   Just invokes a system call that shuts down the OS. */
+
+#include <syscall.h>
+#include <stdio.h>
+
+int
+main (void)
+{
+  printf("Running Halt\n");
+  halt();
+  /* not reached */
+}
